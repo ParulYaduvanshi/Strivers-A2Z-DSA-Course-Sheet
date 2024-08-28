4cf8980dcb2f6377f11cd67f0f8620ed049d11a1
Parul Yaduvanshi <117031532+ParulYaduvanshi@users.noreply.github.com>
2024-08-28 10:15:02 +0530

    Add files via upload

---
+++ Step 2: Learn Important Sorting Techniques/insertion_sort.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+void insertion(int arr[], int n)
+{
+    for (int i = 0; i <= n - 1; i++)
+    {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j])
+        {
+            swap(arr[j - 1], arr[j]);
+            j--;
+        }
+    }
+
+    cout << "Sorted Array: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Before Sorting Array elements are: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    // insertion sort
+    insertion(arr, n);
+
+    return 0;
+}
