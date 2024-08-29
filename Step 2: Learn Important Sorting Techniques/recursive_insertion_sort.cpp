fdde4af110b22bfd2b91458b1e2d0a9beec551cc
Parul Yaduvanshi <117031532+ParulYaduvanshi@users.noreply.github.com>
2024-08-29 20:51:54 +0530

    Add files via upload

---
+++ Step 2: Learn Important Sorting Techniques/recursive_insertion_sort.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+void insertion_sort(int arr[], int i, int n) {
+
+    // Base Case: i == n.
+    if (i == n) return;
+
+    int j = i;
+    while (j > 0 && arr[j - 1] > arr[j]) {
+        int temp = arr[j - 1];
+        arr[j - 1] = arr[j];
+        arr[j] = temp;
+        j--;
+    }
+
+    insertion_sort(arr, i + 1, n);
+}
+int main(){
+    int n;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+
+    int arr[n];
+    cout<<"Enter the elements of the array: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    insertion_sort(arr, 0, n);
+    cout<<"Sorted array: ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
