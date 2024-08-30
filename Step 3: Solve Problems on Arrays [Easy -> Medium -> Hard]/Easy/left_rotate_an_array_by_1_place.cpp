1577c75c3d6e67cbd72342411156ac5d71a9c197
Parul Yaduvanshi <117031532+ParulYaduvanshi@users.noreply.github.com>
2024-08-30 17:52:45 +0530

    Add files via upload

---
+++ Step 3: Solve Problems on Arrays [Easy -> Medium -> Hard]/Easy/left_rotate_an_array_by_1_place.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+void left_rotate(int arr[],int n){
+    int temp[n];
+    for (int i=1;i<n;i++){
+        temp[i-1]=arr[i];
+    }
+    temp[n-1]=arr[0];
+    for(int i=0;i<n;i++){
+        cout<<temp[i]<<" ";
+    }
+    
+}
+int main(){
+    int n;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+
+    int arr[n];
+    cout<<"Enter the elements of the array: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    left_rotate(arr,n);
+
+    return 0;
+}
