9a90fe26450e3127a8ae70ac3f0a8d9c77bf1905
Parul Yaduvanshi <117031532+ParulYaduvanshi@users.noreply.github.com>
2024-09-03 16:37:59 +0530

    Add files via upload

---
+++ Step 4: Binary Search [1D, 2D Arrays, Search Space]/find_out_how_many_times_an_array_has_been_rotated.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int times(vector<int> &arr,int n){
+    int s=0;
+    int e=n-1;
+    while(s<e){
+        int mid=s+(e-s)/2;
+        if (arr[mid]>arr[e]){
+            s=mid+1;
+        }
+        else{
+            e=mid;
+        }
+    }
+    return s;
+}
+int main(){
+    int n;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+
+    vector<int>arr(n);
+    cout<<"Enter the elements of the array: ";
+    for (int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    cout<<times(arr,n);
+
+    return 0;
+}
