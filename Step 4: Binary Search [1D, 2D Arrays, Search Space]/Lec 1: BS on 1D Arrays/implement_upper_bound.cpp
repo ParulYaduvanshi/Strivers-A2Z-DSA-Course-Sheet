175eb6830c09cdabacbd4020fd1a0bbeb4e47090
Parul Yaduvanshi <117031532+ParulYaduvanshi@users.noreply.github.com>
2024-09-04 18:25:33 +0530

    Add files via upload

---
+++ Step 4: Binary Search [1D, 2D Arrays, Search Space]/Lec 1: BS on 1D Arrays/implement_upper_bound.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+int upper_bound(int arr[], int n, int k)
+{
+    int s = 0;
+    int e = n - 1;
+    int ans=n;
+    while (s <= e)
+    {
+        int mid = s + (e - s) / 2;
+        if (arr[mid] > k)
+        {
+            ans=mid;
+            e=mid-1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+        
+    }
+    return ans;
+}
+int main()
+{
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int k;
+    cout << "Enter the element to search: ";
+    cin >> k;
+
+    int ans = upper_bound(arr, n, k);
+    cout<<"Upper Bound is at the index: "<<ans<<endl;
+
+    return 0;
+}
